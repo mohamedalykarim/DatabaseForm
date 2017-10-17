dd490ad4be4d0ade33abb0d798b1cb86ba17905d
mohamedalykarim <mohamed.aly.karim@gmail.com>
2017-10-17 22:23:03 +0200

    First Commit

---
+++ frm.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Form Name</title>
+    <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+</head>
+
+
+<style>
+    body{
+        Background: #eceef4;
+        font-family: 'Raleway', sans-serif;
+    }
+
+    .main-container{
+        width: 60%;
+        height: auto;
+        margin: auto;
+    }
+
+    .form-container{
+        background: #ffffff;
+        -moz-border-radius: 7px;
+        -webkit-border-radius: 7px;
+        border-radius: 7px;
+    }
+
+
+</style>
+<body>
+
+<div class="main-container">
+    <h2 style="color: #505050; margin-left: 10px;">SHIPPING AND BILLING FORM</h2>
+
+    <div class="form-container">
+        <form>
+
+        </form>
+
+        <br><br><br><br><br>
+    </div>
+</div>
+
+
+
+</body>
+</html>
